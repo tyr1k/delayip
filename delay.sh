01e57386b071bd627f224a4629da9422f27903a0
tyr1k <65919376+tyr1k@users.noreply.github.com>
2023-03-19 11:39:44 +0300

    Update delay.sh
    update:
    Add some error checking:
    
    Check if the user has entered the correct number of arguments.
    Check if the entered interface name and IP address are valid.
    Use variables to store the commands and options to make the script easier to read and maintain.
    
    Add some error messages to help users understand what went wrong.

--- delay.sh
+++ delay.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 
-if [ "$1" = "-h" ] || [ $1 = "--help" ] || [ "$1" = "" ]; then
-  echo "Enter: network interfaces, ip-address whom need to make delay, time delay(ms)"
-  echo "Example: ./delayip.sh eth0 192.168.1.1 10ms
+if [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ "$#" -ne 3 ]; then
+  echo "Usage: $0 <interface> <ip-address> <delay>"
+  echo "Example: $0 eth0 192.168.1.1 10ms"
   echo "--off(-o) - reset delay"
- exit
- fi
- 
- if [  "$1" = "-o" ] || [ "$1" = "--off" ]; then
+  exit 1
+fi
+
+if [ "$1" = "-o" ] || [ "$1" = "--off" ]; then
   tc qdisc del dev eth0 root
   echo "Delay reset"
- exit
- fi
- 
- eth=$3
- 
- if [ "eth" = ""]; then
-  eth="eth0"
- fi
- 
- tc qdisc del dev $eth root
- tc qdisc add dev $eth root handle 1: htb
- tc class add dev $eth parent 1: classid 1:1 htb rate 100mbit
- tc filter add dev $eth parent 1: protocol ip prio 1 u32 flowid 1:1 match ip dst $1
- tc qdisc add dev $eth parent 1:1 handle 10: netem delay $2
- 
- echo "For $eth $1 set delay to $2"
+  exit 0
+fi
+
+interface="$1"
+ip_address="$2"
+delay="$3"
+
+if ! ip link show "$interface" &> /dev/null; then
+  echo "Error: Invalid interface name $interface"
+  exit 1
+fi
+
+if ! ping -c 1 -w 1 "$ip_address" &> /dev/null; then
+  echo "Error: Invalid IP address $ip_address"
+  exit 1
+fi
+
+tc qdisc del dev "$interface" root
+tc qdisc add dev "$interface" root handle 1: htb
+tc class add dev "$interface" parent 1: classid 1:1 htb rate 100mbit
+tc filter add dev "$interface" parent 1: protocol ip prio 1 u32 flowid 1:1 match ip dst "$ip_address"
+tc qdisc add dev "$interface" parent 1:1 handle 10: netem delay "$delay"
+
+echo "For $interface $ip_address set delay to $delay"
